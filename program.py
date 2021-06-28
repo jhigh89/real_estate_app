def63dc0b25c6074849585a100deca8cf1fb17a3
Jacob Hightower <jah795@nau.edu>
2021-06-27 22:28:53 -0700

    finished app, more needed: list comp, gen method

--- program.py
+++ program.py
@@ -1,12 +1,13 @@
 import os
 import csv
+from data_types import Purchase
+import statistics
 
 path = "C:\\Repos\\real_estate_app\\data\\SacramentoRealEstateTranscations2008.csv"
 
 def main():
     print_header()
     filename = get_data_file()
-    print(filename)
     data = load_file(filename)
     query_data(data)
 
@@ -23,10 +24,12 @@ def load_file(filename):
     with open(filename,'r',encoding="utf-8") as file:
 
         reader = csv.DictReader(file)
+        purchases = []
         for row in reader:
-            print(row)
-            print(f"Price {row['price']}")
+            p = Purchase.create_from_dict(row)
+            purchases.append(p)
         
+        return purchases
 
         # header = file.readline().strip()
         # reader = csv.reader(file)
@@ -45,8 +48,43 @@ def load_file_basic(filename):
 
         print(lines[:3])
 
-def query_data(data):
-    pass
+def get_price(p):
+    return p.price
+
+def query_data(data: list[Purchase]):
+
+    data.sort(key=lambda p: p.price)
+
+    
+    high_purchase = data[-1]
+    print(f"The most expensive house cost: ${high_purchase.price:.2f}. It contained {high_purchase.beds} bedrooms and {high_purchase.baths} bathrooms.")
+    # least expensive house?
+    low_purchase = data[0]
+    print(f"The least expensive house cost: ${low_purchase.price:.2f}. It contained {low_purchase.beds} bedrooms and {low_purchase.baths} bathrooms.")
+
+    prices = [
+        p.price # projection or items
+        for p in data # the set to process
+    ]
+
+    ave_price = statistics.mean(prices)
+    print(f"The average home price was ${ave_price:.2f}.")
+
+    prices = [
+        p # projection or items
+        for p in data # the set to process
+        if p.beds == 2 # test / condition
+    ]
+
+    ave_2b_price = statistics.mean(p.price for p in prices)
+    ave_2b_bath = statistics.mean(p.baths for p in prices)
+    ave_2b_sqft = statistics.mean(p.sq__ft for p in prices)
+    print(f"Average 2 bedroom house was ${ave_2b_price:.2f}, baths={ave_2b_bath:.2f}, sqft={ave_2b_sqft:.2f}.")
+
+def announce(item, msg):
+    print(f"Pulling item {item} for {msg}")
+    return item
+    
 
 if __name__ == '__main__':
     main()
